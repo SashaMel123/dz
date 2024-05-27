5509678db3949c0eb9a1357bca7af920b55d30ba
SashaMel123 <160107032+SashaMel123@users.noreply.github.com>
2024-05-27 22:19:46 +0300

    Add files via upload

---
+++ Task 5/form.php
@@ -0,0 +1,5 @@
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="text" name="fileName" placeholder="Введіть назву файлу">
+    <input type="submit" name="submit" value="Завантажити файл">
+</form>
